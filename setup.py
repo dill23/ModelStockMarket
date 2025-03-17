f87850076b6b4037f42a6e7a390f380ad2e50de4
dlhil <dlhilse04@gmail.com>
2025-03-16 21:57:43 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ModalStockMarket",
+    author="dill23",
+    version="0.0.0",
+    license="GNU",
+    url="https://github.com/dill23/ModelStockMarket"
+)
